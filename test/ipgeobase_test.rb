68fd718475163b146f35f1dcf4a1e74650480218
gor <7495500@gmail.com>
2022-08-11 16:35:38 +0300

    Add tests with webmock/minitest

--- test/ipgeobase_test.rb
+++ test/ipgeobase_test.rb
@@ -1,13 +1,22 @@
 # frozen_string_literal: true
 
-require "test_helper"
+require_relative "test_helper"
+require_relative "../lib/ipgeobase"
 
 class IpgeobaseTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Ipgeobase::VERSION
+  def test_api_url
+    body = puts "\napi_url:\t200\n"
+    stub_request(:get, "http://www.ip-api.com/").to_return(status: 200, body: body)
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_xml_data_get
+    body = puts "\nxml_data_get:\t200\n"
+
+    uri_template = Addressable::Template.new("http://{host}{/segments*}{?fields}").expand({
+      'host'     => 'ip-api.com',
+      'segments' => ['xml', "81.200.23.8"],
+      'fields'   => 'country,countryCode,city,lat,lon'
+    })
+    stub_request(:any, uri_template).to_return(status: 200, body: body)
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,8 @@
 
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "ipgeobase"
-
 require "minitest/autorun"
 require 'webmock/minitest'
+
+include WebMock::API
+WebMock.enable!
