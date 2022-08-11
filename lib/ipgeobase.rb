90ceaea0e0e578b7657c94696d018082b3e0862e
gor <7495500@gmail.com>
2022-08-11 16:46:20 +0300

    Add app logic

--- lib/ipgeobase.rb
+++ lib/ipgeobase.rb
@@ -1,8 +1,24 @@
 # frozen_string_literal: true
 
 require_relative "ipgeobase/version"
+require "addressable/template"
+require 'happymapper'
+require 'net/http'
+
 
 module Ipgeobase
   class Error < StandardError; end
-  # Your code goes here...
+
+  class << self
+    def lookup(ip)
+      lookup_uri = Addressable::Template.new("http://{host}{/segments*}{?fields}").expand({
+        'host'     => 'ip-api.com',
+        'segments' => ['xml', "#{ip}"],
+        'fields'   => 'country,countryCode,city,lat,lon'
+      })
+
+      lookup_xml = Net::HTTP.get lookup_uri
+      HappyMapper.parse lookup_xml
+    end
+  end
 end
