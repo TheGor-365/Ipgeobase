82e795f6fb4d2380173014957faed769d479fbac
gor <7495500@gmail.com>
2022-08-12 21:21:47 +0300

    final rubocop refactoring

--- lib/ipgeobase.rb
+++ lib/ipgeobase.rb
@@ -12,11 +12,11 @@ module Ipgeobase
   class << self
     def lookup(ip)
       lookup_uri = Addressable::Template.new("http://{host}{/segments*}{?fields}")
-      .expand({
-                "host" => "ip-api.com",
-                "segments" => ["xml", ip.to_s],
-                "fields" => "country,countryCode,city,lat,lon"
-              })
+                                        .expand({
+                                                  "host" => "ip-api.com",
+                                                  "segments" => ["xml", ip.to_s],
+                                                  "fields" => "country,countryCode,city,lat,lon"
+                                                })
       lookup_xml = Net::HTTP.get lookup_uri
       HappyMapper.parse lookup_xml
     end
