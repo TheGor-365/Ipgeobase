6528c6d1daba13d05ba58b8b7f498ad448152b42
gor <7495500@gmail.com>
2022-08-25 17:21:20 +0300

    add constant for uri

--- lib/ipgeobase.rb
+++ lib/ipgeobase.rb
@@ -1,22 +1,22 @@
 # frozen_string_literal: true
 
 require_relative "ipgeobase/version"
+require_relative "ipgeobase/address"
 require "addressable/template"
-require "happymapper"
 require "net/http"
 
 # Service to get location info by ip
 module Ipgeobase
   class << self
+    API_URL = "http://ip-api.com"
+
     def lookup(ip)
-      lookup_uri = Addressable::Template.new("http://{host}{/segments*}{?fields}")
-                                        .expand({
-                                                  "host" => "ip-api.com",
-                                                  "segments" => ["xml", ip.to_s],
-                                                  "fields" => "country,countryCode,city,lat,lon"
-                                                })
-      lookup_xml = Net::HTTP.get lookup_uri
-      HappyMapper.parse lookup_xml
+      uri = Addressable::Template.new("#{API_URL}{/segments*}{?fields}").expand({
+        "segments" => ["xml", ip.to_s],
+        "fields" => "country,countryCode,city,lat,lon"
+      })
+      xml = Net::HTTP.get uri
+      Address.parse xml
     end
   end
 end
