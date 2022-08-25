c9f75ac9d077e07ba4b7f911c62bf4f8d653a934
gor <7495500@gmail.com>
2022-08-25 21:53:37 +0300

    refactoring stub

--- test/ipgeobase_test.rb
+++ test/ipgeobase_test.rb
@@ -5,10 +5,10 @@ require_relative "../lib/ipgeobase"
 
 class IpgeobaseTest < Minitest::Test
   include WebMock::API
-  API_URL = "http://ip-api.com/xml/81.200.23.8?country,countryCode,city,lat,lon"
+  GET_XML_INFO = "http://ip-api.com/xml/81.200.23.8?fields=country,countryCode,city,lat,lon"
 
-  def test_xml_data_get
-    request = URI.parse API_URL
-    stub_request(:any, request).to_return(status: 200, body: "hey")
+  def test_xml_data
+    body = ''
+    stub_request(:get, GET_XML_INFO).to_return(status: 200, body: body)
   end
 end
