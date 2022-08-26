09f58031a915c1b7c8d6d036d83ad26a30e82a33
gor <7495500@gmail.com>
2022-08-26 15:07:04 +0300

    add stup snippet to ipgeobase_test.rb

--- test/ipgeobase_test.rb
+++ test/ipgeobase_test.rb
@@ -4,11 +4,16 @@ require_relative "test_helper"
 require_relative "../lib/ipgeobase"
 
 class IpgeobaseTest < Minitest::Test
-  include WebMock::API
   GET_XML_INFO = "http://ip-api.com/xml/81.200.23.8?fields=country,countryCode,city,lat,lon"
 
-  def test_xml_data
-    body = ''
-    stub_request(:get, GET_XML_INFO).to_return(status: 200, body: body)
+  def get_xml
+    stub_request(:get, GET_XML_INFO).
+    with(
+      headers: {
+        'Accept'=>'*/*',
+        'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+        'User-Agent'=>'Ruby'
+        }).
+        to_return(status: 200, body: "", headers: {})
   end
 end
