cb7620cb68f5c54327959d61702d584fca85c756
Gor <7495500@gmail.com>
2022-09-07 13:56:58 +0300

    test_lookup code refactoring

--- test/ipgeobase_test.rb
+++ test/ipgeobase_test.rb
@@ -7,7 +7,7 @@ class IpgeobaseTest < Minitest::Test
   GET_XML_INFO = "http://ip-api.com/xml/81.200.23.8?fields=country,countryCode,city,lat,lon"
 
   def test_lookup
-    body = '<?xml version="1.0" encoding="UTF-8"?>
+    xml = '<?xml version="1.0" encoding="UTF-8"?>
             <query>
               <country>Russia</country>
               <countryCode>RU</countryCode>
@@ -16,13 +16,14 @@ class IpgeobaseTest < Minitest::Test
               <lon>37.6171</lon>
             </query>'
 
-    stub_request(:get, GET_XML_INFO).to_return(status: 200, body: body)
-    address = Address.parse(body)
+    stub_request(:get, GET_XML_INFO).to_return(status: 200, body: xml)
+    
+    address = Address.parse(xml)
 
     assert_equal 'Russia', address.country
     assert_equal 'Moscow', address.city
-    assert_equal 'RU', address.country_code
-    assert_equal 55.7483, address.lat
-    assert_equal 37.6171, address.lon
+    assert_equal 'RU',     address.country_code
+    assert_equal 55.7483,  address.lat
+    assert_equal 37.6171,  address.lon
   end
 end
